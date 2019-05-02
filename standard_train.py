58c2a82c768b482c1ec52869055f7bcdf8e90cc2
rajasekhar <rajasekhar@swecha.net>
2019-05-02 09:31:00 +0700

    Update standard_train.py

--- standard_train.py
+++ standard_train.py
@@ -112,6 +112,11 @@ def train_model(model, dataloaders, criterion, optimizer, num_epochs=25, is_ince
     # load best model weights
     model.load_state_dict(best_model_wts)
     return model, val_acc_history
+
+def set_parameter_requires_grad(model, feature_extracting):
+    if feature_extracting:
+        for param in model.parameters():
+            param.requires_grad = False
     
     
 def initialize_model(model_name, num_classes, feature_extract, use_pretrained=True):
